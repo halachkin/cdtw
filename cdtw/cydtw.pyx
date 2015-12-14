54332307f08c7535ff5243d86c91cad5b76eae5b
Johannes Hugo Kitschke <j.kitschke@gmx.de>
2015-12-14 14:14:47 +0100

    Move step strings into step class

--- cdtw/cydtw.pyx
+++ cdtw/cydtw.pyx
@@ -7,122 +7,6 @@ cimport numpy as np
 np.import_array()
 
 #from cdtw.stepstr import *
-stepstr = dict()
-
-stepstr['dp1'] = "\n \
- *  Step patern dp1: \n \
- *  min(\n \
- *       cost_matrix[i][j-1]   +   d(r[i],q[j]) \n \
- *       cost_matrix[i-1][j]   +   d(r[i],q[j]), \n \
- *       cost_matrix[i-1][j-1] + 2*d(r[i],q[j]) \n \
- *      )\n \
- "
-
-stepstr['dp2'] = " \n \
- *  Step patern dp2:\n \
- *  min(\n \
- *      cost_matrix[i][j-1]   +   d(r[i],q[j]) \n \
- *      cost_matrix[i-1][j]   +   d(r[i],q[j]),\n \
- *      cost_matrix[i-1][j-1] +   d(r[i],q[j]) \n \
- *      )\n \
- "
-
-stepstr['dp3'] = "\n \
- *  Step patern dp3:\n \
- *  min(      \n \
- *      cost_matrix[i][j-1]   +   d(r[i],q[j]) \n \
- *      cost_matrix[i-1][j]   +   d(r[i],q[j]),\n \
- *      )\n \
- "
-
-stepstr['p0sym'] = "\n \
- *  Sakoe-Chiba classification p = 0, symmetric step pattern:\n \
- *  min(      \n \
- *      cost_matrix[i][j-1]   +   d(r[i],q[j]) \n \
- *      cost_matrix[i-1][j]   +   d(r[i],q[j]),\n \
- *      )\n \
- "
-
-stepstr['p0asym'] = "\n \
- *  Sakoe-Chiba classification p = 0, asymmetric step pattern: \n \
- *  min(      \n \
- *       cost_matrix[i][j-1]   +   0   \n \
- *       cost_matrix[i-1][j]   +   d(r[i],q[j]), \n \
- *       cost_matrix[i-1][j-1] +   d(r[i],q[j]), \n \
- *      )\n \
- "
-
-
-stepstr['p05sym'] = "\n \
- * Sakoe-Chiba classification p = 0.5, symmetric step pattern: \n \
- *  min( \n \
- *      cost_matrix[i-1][j-3] + 2d(r[i],q[j-2]) + d(r[i],q[j-1]) + d(r[i],q[j]),\n \
- *      cost_matrix[i-1][j-2] + 2d(r[i],q[j-1]) + d(r[i],q[j]), \n \
- *      cost_matrix[i-1][j-1] + 2d(r[i],q[j]), \n \
- *      cost_matrix[i-2][j-1] + 2d(r[i-1],q[j]) + d(r[i],q[j]), \n \
- *      cost_matrix[i-3][j-1] + 2d(r[i-2],q[j]) + d(r[i-1],q[j]) + d(r[i],q[j]) \n \
- *      )"
-
-
-
-stepstr['p05asym'] =  "\n \
- * Sakoe-Chiba classification p = 0.5, asymmetric step pattern:\n \
- *  min( \n \
- *   cost_matrix[i-1][j-3] + (d(r[i],q[j-2]) + d(r[i],q[j-1]) + d(r[i],q[j]))/3 \n \
- *   cost_matrix[i-1][j-2] + (d(r[i],q[j-1]) + d(r[i],q[j]))/2, \n \
- *   cost_matrix[i-1][j-1] + d(r[i],q[j]), \n \
- *   cost_matrix[i-2][j-1] + d(r[i-1],q[j])  + d(r[i],q[j]),\n \
- *   cost_matrix[i-3][j-1] + d(r[i-2],q[j])  + d(r[i-1],q[j]) + d(r[i],q[j]) \n \
- *      )"
-
-
-stepstr['p1sym'] = "\n \
- * Sakoe-Chiba classification p = 1, symmetric step pattern:\n \
- *  min(        \n \
- *      cost_matrix[i-1][j-2] + 2d(r[i],q[j-1]) + d(r[i],q[j]), \n \
- *      cost_matrix[i-1][j-1] + 2d(r[i],q[j]), \n \
- *      cost_matrix[i-2][j-1] + 2d(r[i-1],q[j]) + d(r[i],q[j]), \n \
- *      )"
-
-stepstr['p1asym'] = "\n \
- * Sakoe-Chiba classification p = 1, asymmetric step pattern:\n \
- *  min( \n \
- *      cost_matrix[i-1][j-2] + (d(r[i],q[j-1]) + d(r[i],q[j]))/2, \n \
- *      cost_matrix[i-1][j-1] + d(r[i],q[j]), \n \
- *      cost_matrix[i-2][j-1] + d(r[i-1],q[j]) + d(r[i],q[j]), \n \
- *      ) \n \
-" 
-
-stepstr['p2sym'] = "\n \
- * Sakoe-Chiba classification p = 2, symmetric step pattern: \n \
- *  min(   \n \
- *     cost_matrix[i-2][j-3] + 2d(r[i-1],q[j-2]) + \n \
- *                             2d(r[i],q[j-1])   + \n \
- *                             d(r[i],q[j]), \n \
- * \n \
- *     cost_matrix[i-1][j-1] + 2d(r[i],q[j]), \n \
- * \n \
- *     cost_matrix[i-3][j-2] + 2d(r[i-2],q[j-1]) + \n \
- *                             2d(r[i-1],q[j])   + \n \
- *                             d(r[i],q[j]) \n \
- *     ) \n \
-"
-
-
-stepstr['p2asym'] = " \n \
- * Sakoe-Chiba classification p = 2, asymmetric step pattern: \n \
- *  min(   \n \
- *     cost_matrix[i-2][j-3] + 2( d(r[i-1],q[j-2]) +  \n \
- *                                d(r[i],q[j-1])   +  \n \
- *                                d(r[i],q[j]) ),     \n \
- * \n \
- *     cost_matrix[i-1][j-1] + d(r[i],q[j]), \n \
- * \n \
- *     cost_matrix[i-3][j-2] + d(r[i-2],q[j-1]) +    \n \
- *                             d(r[i-1],q[j])   +    \n \
- *                             d(r[i],q[j])          \n \
- *     )" 
-
 # import c fuctions
 cdef extern from "cdtw.c":
 
@@ -284,6 +168,122 @@ class Step(Setting):
                        'p2sym': _SCP2SYM, 'p2asym': _SCP2ASYM}
         self._weights = weights
 
+        self.stepstr = dict()
+        
+        self.stepstr['dp1'] = "\n \
+         *  Step patern dp1: \n \
+         *  min(\n \
+         *       cost_matrix[i][j-1]   +   d(r[i],q[j]) \n \
+         *       cost_matrix[i-1][j]   +   d(r[i],q[j]), \n \
+         *       cost_matrix[i-1][j-1] + 2*d(r[i],q[j]) \n \
+         *      )\n \
+         "
+        
+        self.stepstr['dp2'] = " \n \
+         *  Step patern dp2:\n \
+         *  min(\n \
+         *      cost_matrix[i][j-1]   +   d(r[i],q[j]) \n \
+         *      cost_matrix[i-1][j]   +   d(r[i],q[j]),\n \
+         *      cost_matrix[i-1][j-1] +   d(r[i],q[j]) \n \
+         *      )\n \
+         "
+        
+        self.stepstr['dp3'] = "\n \
+         *  Step patern dp3:\n \
+         *  min(      \n \
+         *      cost_matrix[i][j-1]   +   d(r[i],q[j]) \n \
+         *      cost_matrix[i-1][j]   +   d(r[i],q[j]),\n \
+         *      )\n \
+         "
+        
+        self.stepstr['p0sym'] = "\n \
+         *  Sakoe-Chiba classification p = 0, symmetric step pattern:\n \
+         *  min(      \n \
+         *      cost_matrix[i][j-1]   +   d(r[i],q[j]) \n \
+         *      cost_matrix[i-1][j]   +   d(r[i],q[j]),\n \
+         *      )\n \
+         "
+        
+        self.stepstr['p0asym'] = "\n \
+         *  Sakoe-Chiba classification p = 0, asymmetric step pattern: \n \
+         *  min(      \n \
+         *       cost_matrix[i][j-1]   +   0   \n \
+         *       cost_matrix[i-1][j]   +   d(r[i],q[j]), \n \
+         *       cost_matrix[i-1][j-1] +   d(r[i],q[j]), \n \
+         *      )\n \
+         "
+        
+        
+        self.stepstr['p05sym'] = "\n \
+         * Sakoe-Chiba classification p = 0.5, symmetric step pattern: \n \
+         *  min( \n \
+         *      cost_matrix[i-1][j-3] + 2d(r[i],q[j-2]) + d(r[i],q[j-1]) + d(r[i],q[j]),\n \
+         *      cost_matrix[i-1][j-2] + 2d(r[i],q[j-1]) + d(r[i],q[j]), \n \
+         *      cost_matrix[i-1][j-1] + 2d(r[i],q[j]), \n \
+         *      cost_matrix[i-2][j-1] + 2d(r[i-1],q[j]) + d(r[i],q[j]), \n \
+         *      cost_matrix[i-3][j-1] + 2d(r[i-2],q[j]) + d(r[i-1],q[j]) + d(r[i],q[j]) \n \
+         *      )"
+        
+        
+        
+        self.stepstr['p05asym'] =  "\n \
+         * Sakoe-Chiba classification p = 0.5, asymmetric step pattern:\n \
+         *  min( \n \
+         *   cost_matrix[i-1][j-3] + (d(r[i],q[j-2]) + d(r[i],q[j-1]) + d(r[i],q[j]))/3 \n \
+         *   cost_matrix[i-1][j-2] + (d(r[i],q[j-1]) + d(r[i],q[j]))/2, \n \
+         *   cost_matrix[i-1][j-1] + d(r[i],q[j]), \n \
+         *   cost_matrix[i-2][j-1] + d(r[i-1],q[j])  + d(r[i],q[j]),\n \
+         *   cost_matrix[i-3][j-1] + d(r[i-2],q[j])  + d(r[i-1],q[j]) + d(r[i],q[j]) \n \
+         *      )"
+        
+        
+        self.stepstr['p1sym'] = "\n \
+         * Sakoe-Chiba classification p = 1, symmetric step pattern:\n \
+         *  min(        \n \
+         *      cost_matrix[i-1][j-2] + 2d(r[i],q[j-1]) + d(r[i],q[j]), \n \
+         *      cost_matrix[i-1][j-1] + 2d(r[i],q[j]), \n \
+         *      cost_matrix[i-2][j-1] + 2d(r[i-1],q[j]) + d(r[i],q[j]), \n \
+         *      )"
+        
+        self.stepstr['p1asym'] = "\n \
+         * Sakoe-Chiba classification p = 1, asymmetric step pattern:\n \
+         *  min( \n \
+         *      cost_matrix[i-1][j-2] + (d(r[i],q[j-1]) + d(r[i],q[j]))/2, \n \
+         *      cost_matrix[i-1][j-1] + d(r[i],q[j]), \n \
+         *      cost_matrix[i-2][j-1] + d(r[i-1],q[j]) + d(r[i],q[j]), \n \
+         *      ) \n \
+        " 
+        
+        self.stepstr['p2sym'] = "\n \
+         * Sakoe-Chiba classification p = 2, symmetric step pattern: \n \
+         *  min(   \n \
+         *     cost_matrix[i-2][j-3] + 2d(r[i-1],q[j-2]) + \n \
+         *                             2d(r[i],q[j-1])   + \n \
+         *                             d(r[i],q[j]), \n \
+         * \n \
+         *     cost_matrix[i-1][j-1] + 2d(r[i],q[j]), \n \
+         * \n \
+         *     cost_matrix[i-3][j-2] + 2d(r[i-2],q[j-1]) + \n \
+         *                             2d(r[i-1],q[j])   + \n \
+         *                             d(r[i],q[j]) \n \
+         *     ) \n \
+        "
+        
+        
+        self.stepstr['p2asym'] = " \n \
+         * Sakoe-Chiba classification p = 2, asymmetric step pattern: \n \
+         *  min(   \n \
+         *     cost_matrix[i-2][j-3] + 2( d(r[i-1],q[j-2]) +  \n \
+         *                                d(r[i],q[j-1])   +  \n \
+         *                                d(r[i],q[j]) ),     \n \
+         * \n \
+         *     cost_matrix[i-1][j-1] + d(r[i],q[j]), \n \
+         * \n \
+         *     cost_matrix[i-3][j-2] + d(r[i-2],q[j-1]) +    \n \
+         *                             d(r[i-1],q[j])   +    \n \
+         *                             d(r[i],q[j])          \n \
+         *     )" 
+
 
 
     def set_weights(self,w):
@@ -293,7 +293,7 @@ class Step(Setting):
         return self._weights
 
     def step_str(self, itype):
-        return stepstr[itype]
+        return self.stepstr[itype]
 
     def __str__(self):
         return str(self._cur_type)
